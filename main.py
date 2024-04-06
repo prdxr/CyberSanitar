49d74a40723022021070e55794d9328578dcaa38
prdx <crazy.paradoxer@gmail.com>
2024-04-06 20:53:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This example requires the 'message_content' intent.
+
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+TOKEN = 'token here'
+client = MyClient(intents=intents)
+client.run(TOKEN)
