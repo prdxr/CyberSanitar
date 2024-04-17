1dc1c870b00a54237e88d32e09ddd18cc8af2293
prdx <crazy.paradoxer@gmail.com>
2024-04-17 23:30:46 +0300

    feat: add data folder, add paths to global settings, add config file

--- settings_module/_global.py
+++ settings_module/_global.py
@@ -1,3 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+SETTINGS_MODULE = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(SETTINGS_MODULE)
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
