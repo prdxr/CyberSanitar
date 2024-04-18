f2d34f4b962ce153e90e983d0def4c391f198cd2
prdx <crazy.paradoxer@gmail.com>
2024-04-18 18:10:24 +0300

    fix: move admin and mod check into separate utils.py file

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,22 +1,7 @@
 import discord
 from discord.ext import commands
-from discord.embeds import Embed
-from settings import ADMIN_ROLE_ID, MODERATOR_ROLE_ID
 
-
-def check_admin():
-    def predicate(ctx):
-        return commands.check_any(commands.is_owner(), commands.has_role(ADMIN_ROLE_ID))
-    print(predicate(None))
-    return commands.check(predicate)
-
-
-def check_mod():
-    def predicate(ctx):
-        return commands.check_any(commands.is_owner(),
-                                  commands.has_role(ADMIN_ROLE_ID),
-                                  commands.has_role(MODERATOR_ROLE_ID))
-    return commands.check(predicate)
+from utils import check_admin, check_mod
 
 
 class Admin(commands.Cog):
